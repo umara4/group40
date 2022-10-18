077aa80dfb3ec7afe8ae1cad3950c952eca6b976
kjingruz <58500251+kjingruz@users.noreply.github.com>
2022-10-17 21:07:43 -0400

    Add files via upload
    Register and Login done, Welcome page post Login partially done, not done the mode and edit page in the post login page.

---
+++ finalversion.py
@@ -0,0 +1,258 @@
+import tkinter
+import tkinter.ttk
+import tkinter.messagebox
+import sqlite3
+
+warning = True
+connection = True
+
+
+class Database:
+    def __init__(self):
+        self.dbConnection = sqlite3.connect("patientdb.db")
+        self.dbCursor = self.dbConnection.cursor()
+        self.dbCursor.execute(
+            "CREATE TABLE IF NOT EXISTS patient_table (firstname text, lastname text, password text)")
+
+    def __del__(self):
+        self.dbCursor.close()
+        self.dbConnection.close()
+
+    def Insert(self, firstname, lastname, password):
+        self.dbCursor.execute("INSERT INTO patient_table VALUES (?, ?, ?)",
+                              (firstname, lastname, password))
+        self.dbConnection.commit()
+
+    def Search(self,lastname,password):
+        self.dbCursor.execute("SELECT * FROM patient_table WHERE lastname = ? and password = ?",
+                              (lastname,password))
+        searchResults = self.dbCursor.fetchall()
+        return searchResults
+
+    def Display(self):
+        self.dbCursor.execute("SELECT * FROM patient_table")
+        records = self.dbCursor.fetchall()
+        return records
+
+    def Check(self):
+        self.dbCursor.execute("SELECT COUNT(*) FROM patient_table")
+        result = self.dbCursor.fetchone()
+        return result
+
+class Values:
+    def Validate(self, firstname, lastname, password, passwordreentry):
+        if not (firstname.isalpha()):
+            return "firstname"
+        elif not (lastname.isalpha()):
+            return "lastname"
+        elif password != passwordreentry:
+            return "password does not match"
+        else:
+            return "SUCCESS"
+
+
+class RegisterationWindow:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.window.wm_title("Registration")
+        bg_color = "Blue"
+        fg_color = "white"
+        cha_color = "black"
+
+        self.firstname = tkinter.StringVar()
+        self.lastname = tkinter.StringVar()
+        self.password = tkinter.StringVar()
+        self.passwordreentry = tkinter.StringVar()
+
+        # Labels
+        tkinter.Label(self.window, fg=fg_color, bg=bg_color, text="Patient First Name",
+                      font=("times new roman", 10, "bold"), width=25).grid(pady=5, column=1, row=2)
+        tkinter.Label(self.window, fg=fg_color, bg=bg_color, font=("times new roman", 10, "bold"),
+                      text="Patient Last Name", width=25).grid(pady=5, column=1, row=3)
+        tkinter.Label(self.window, fg=fg_color, bg=bg_color, font=("times new roman", 10, "bold"),
+                      text="Password", width=25).grid(pady=5, column=1, row=4)
+        tkinter.Label(self.window, fg=fg_color, bg=bg_color, font=("times new roman", 10, "bold"),
+                      text="Password Reentry", width=25).grid(pady=5, column=1, row=5)
+
+        self.firstnameEntry = tkinter.Entry(self.window, width=25, textvariable=self.firstname)
+        self.lastnameEntry = tkinter.Entry(self.window, width=25, textvariable=self.lastname)
+        self.passwordEntry = tkinter.Entry(self.window, width=25, textvariable=self.password)
+        self.passwordreEntry = tkinter.Entry(self.window, width=25, textvariable=self.passwordreentry)
+
+        self.firstnameEntry.grid(pady=5, column=3, row=2)
+        self.lastnameEntry.grid(pady=5, column=3, row=3)
+        self.passwordEntry.grid(pady=5, column=3, row=4)
+        self.passwordreEntry.grid(pady=5, column=3, row=5)
+
+        # Button widgets
+        tkinter.Button(self.window, width=10, fg=cha_color, bg=bg_color, font=("times new roman",10,"bold"), text="Insert", command=self.Insert).grid(pady=15, padx=5, column=1,
+                                                                                       row=14)
+        tkinter.Button(self.window, width=10, fg=cha_color, bg=bg_color, font=("times new roman",10,"bold"), text="Reset", command=self.Reset).grid(pady=15, padx=5, column=2, row=14)
+        tkinter.Button(self.window, width=10, fg=cha_color, bg=bg_color, font=("times new roman",10,"bold"), text="Close", command=self.window.destroy).grid(pady=15, padx=5, column=3,
+                                                                                              row=14)
+        self.window.mainloop()
+
+    def Insert(self):
+        self.values = Values()
+        self.database = Database()
+        self.test = self.values.Validate( self.firstnameEntry.get(), self.lastnameEntry.get(),
+                                         self.passwordEntry.get(),self.passwordreEntry.get())
+        if self.test == "SUCCESS":
+            self.database.Insert(self.firstnameEntry.get(), self.lastnameEntry.get(), self.passwordEntry.get())
+            tkinter.messagebox.showinfo("Inserted data", "Successfully inserted the above data in the database")
+        else:
+            self.valueErrorMessage = "Invalid input in field " + self.test
+            tkinter.messagebox.showerror("Value Error", self.valueErrorMessage)
+
+    def Reset(self):
+        self.firstnameEntry.delete(0, tkinter.END)
+        self.lastnameEntry.delete(0, tkinter.END)
+        self.passwordEntry.delete(0,tkinter.END)
+        self.passwordreEntry.delete(0, tkinter.END)
+
+
+class DatabaseView():
+    def __init__(self, data):
+        self.databaseViewWindow = tkinter.Tk()
+        self.databaseViewWindow.wm_title("Database View")
+
+        # Label widgets
+        tkinter.Label(self.databaseViewWindow, text="Database View Window", width=25).grid(pady=5, column=1, row=1)
+
+        self.databaseView = tkinter.ttk.Treeview(self.databaseViewWindow)
+        self.databaseView.grid(pady=5, column=1, row=2)
+        self.databaseView["show"] = "headings"
+        self.databaseView["columns"] = ("firstname", "lastname","Password")
+
+        # Treeview column headings
+        self.databaseView.heading("firstname", text="First Name")
+        self.databaseView.heading("lastname", text="Last Name")
+        self.databaseView.heading("Password", text="Password")
+
+        # Treeview columns
+        self.databaseView.column("firstname", width=100)
+        self.databaseView.column("lastname", width=100)
+        self.databaseView.column("Password",width=100)
+
+        for record in data:
+            self.databaseView.insert('', 'end', values=(record))
+
+        self.databaseViewWindow.mainloop()
+
+
+class LoginWindow():
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.window.wm_title("Login")
+        bg_color = "Blue"
+        fg_color = "white"
+        cha_color = "black"
+
+        self.firstname = tkinter.StringVar()
+        self.lastname = tkinter.StringVar()
+        self.password = tkinter.StringVar()
+
+        tkinter.Label(self.window, fg=fg_color, bg=bg_color, text="Patient First Name",
+                      font=("times new roman", 10, "bold"), width=25).grid(pady=5, column=1, row=2)
+        tkinter.Label(self.window, fg=fg_color, bg=bg_color, font=("times new roman", 10, "bold"),
+                      text="Patient Last Name", width=25).grid(pady=5, column=1, row=3)
+        tkinter.Label(self.window, fg=fg_color, bg=bg_color, font=("times new roman", 10, "bold"),
+                      text="Password", width=25).grid(pady=5, column=1, row=4)
+
+        self.firstnameEntry = tkinter.Entry(self.window, width=25, textvariable=self.firstname)
+        self.lastnameEntry = tkinter.Entry(self.window, width=25, textvariable=self.lastname)
+        self.passwordEntry = tkinter.Entry(self.window, width=25, textvariable=self.password)
+
+        self.firstnameEntry.grid(pady=5, column=3, row=2)
+        self.lastnameEntry.grid(pady=5, column=3, row=3)
+        self.passwordEntry.grid(pady=5, column=3, row=4)
+
+        tkinter.Button(self.window, width=10, fg=cha_color, bg=bg_color, font=("times new roman", 10, "bold"),
+                       text="Submit", command=self.Submit).grid(pady=15, padx=5, column=1,row=14)
+        self.window.mainloop()
+
+    def Submit(self):
+        self.values = Values()
+        self.database = Database()
+        self.data = self.database.Search(self.lastnameEntry.get(), self.passwordEntry.get())
+        if self.data:
+            tkinter.messagebox.showinfo('Login success', "You are logged in!")
+            self.logged_in()
+        else:
+            tkinter.messagebox.showerror("Info not correct")
+
+    def logged_in(self):
+        self.logged_in_window = LoggedInWindow()
+
+
+class LoggedInWindow:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.window.wm_title("Welcome Page")
+        bg_color = "blue"
+        fg_color = "white"
+        cha_color = "black"
+        warning_color = "red"
+        tkinter.Label(self.window, relief=tkinter.GROOVE, fg=fg_color, bg=bg_color, text="Welcome",
+                      font=("times new roman", 20, "bold"), width=30).grid(pady=20, column=1, row=1)
+        tkinter.Button(self.window, width=20, relief=tkinter.GROOVE, fg=cha_color, bg=bg_color, text="Mode",
+                       font=("times new roman", 15, "bold"), command=self.Mode).grid(pady=15, column=1, row=4)
+        tkinter.Button(self.window, width=20, relief=tkinter.GROOVE, fg=cha_color, bg=bg_color, text="Edit",
+                       font=("times new roman", 15, "bold"), command=self.Edit).grid(pady=15, column=1, row=5)
+
+        if warning:
+            tkinter.Label(self.window, relief=tkinter.GROOVE, fg=warning_color, bg=bg_color,
+                          text="Danger! Another Pacemaker nearby!",
+                          font=("times new roman", 10, "bold"), width=50).grid(pady=20, column=1, row=3)
+
+        tkinter.Label(self.window, relief=tkinter.GROOVE, fg=fg_color, bg=bg_color,
+                      text="Status update: Connection with DCM is " + "ON" if (connection == True)
+                      else "Status update: Connection with DCM is " + "OFF",font=("times new roman", 10, "bold"),
+                      width=50).grid(pady=20, column=1, row=2)
+
+    def Mode(self):
+        pass
+
+    def Edit(self):
+        pass
+
+
+class HomePage:
+    def __init__(self):
+        self.homePageWindow = tkinter.Tk()
+        self.homePageWindow.wm_title("Pacemaker Home Page")
+        bg_color = "blue"
+        fg_color = "white"
+        cha_color = "black"
+        lbl_color = 'GREEN'
+        tkinter.Label(self.homePageWindow, relief=tkinter.GROOVE, fg=fg_color, bg=bg_color, text="Home Page",
+                      font=("times new roman",20,"bold"), width=30).grid(pady=20, column=1, row=1)
+        tkinter.Button(self.homePageWindow, width=20, relief=tkinter.GROOVE, fg=cha_color, bg=bg_color, text="Register",
+                       font=("times new roman",15,"bold"), command=self.Register).grid(pady=15, column=1, row=2)
+        tkinter.Button(self.homePageWindow, width=20, relief=tkinter.GROOVE, fg=cha_color, bg=bg_color, text="Login",
+                       font=("times new roman",15,"bold"), command=self.Login).grid(pady=15, column=1, row=3)
+        tkinter.Button(self.homePageWindow, width=20, relief=tkinter.GROOVE, fg=cha_color, bg=bg_color, text="Display",
+                       font=("times new roman",15,"bold"), command=self.Display).grid(pady=15, column=1,row=4)
+        tkinter.Button(self.homePageWindow, width=20, relief=tkinter.GROOVE, fg=cha_color, bg=bg_color, text="Exit",
+                       font=("times new roman",15,"bold"), command=self.homePageWindow.destroy).grid(pady=15,column=1,row=6)
+
+        self.homePageWindow.mainloop()
+
+    def Register(self):
+        self.database = Database()
+        num = self.database.Check()[0]
+        print(num)
+        if num < 10:
+            self.registerWindow = RegisterationWindow()
+        else:
+            tkinter.messagebox.showerror("Registeration Full", "Registeration full, please log in")
+
+    def Display(self):
+        self.database = Database()
+        self.data = self.database.Display()
+        self.displayWindow = DatabaseView(self.data)
+
+    def Login(self):
+        self.loginWindow = LoginWindow()
+
+homePage = HomePage()
